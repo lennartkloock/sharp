fc1badb2689e57c1804b09e068a9b4ceb461283b
Lennart Kloock <lennart.kloock@protonmail.com>
2023-04-10 21:34:57 +0200

    Add custom headers and simple error handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use std::error::Error;
-use std::net::SocketAddr;
-use hyper::service::service_fn;
+use std::{error::Error, net::SocketAddr};
+
+use hyper::{service::service_fn, Body, Response};
+use log::{error, info};
 use tokio::net::{TcpListener, TcpStream};
+
+const VERSION_STRING: &str = concat!(env!("CARGO_PKG_NAME"), " v", env!("CARGO_PKG_VERSION"));
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init();
+
+    info!("{VERSION_STRING}");
 
     let in_addr: SocketAddr = ([127, 0, 0, 1], 3001).into();
     let out_addr: SocketAddr = ([127, 0, 0, 1], 8000).into();
@@ -14,11 +19,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let listener = TcpListener::bind(in_addr).await?;
 
-    println!("Listening on http://{}", in_addr);
-    println!("Proxying on http://{}", out_addr);
+    info!("Listening on http://{}", in_addr);
+    info!("Proxying on http://{}", out_addr);
 
     loop {
-        let (stream, _) = listener.accept().await?;
+        let (stream, client_addr) = listener.accept().await?;
 
         // This is the `Service` that will handle the connection.
         // `service_fn` is a helper to convert a function that
@@ -34,31 +39,51 @@ async fn main() -> Result<(), Box<dyn Error>> {
             );
             let uri = uri_string.parse().unwrap();
             *req.uri_mut() = uri;
+            req.headers_mut().insert(
+                "X-Sharp-Client-Ip",
+                client_addr.ip().to_string().parse().unwrap(),
+            );
+            req.headers_mut().insert(
+                "X-Sharp-Client-Port",
+                client_addr.port().to_string().parse().unwrap(),
+            );
 
             let host = req.uri().host().expect("uri has no host");
             let port = req.uri().port_u16().unwrap_or(80);
             let addr = format!("{}:{}", host, port);
 
             async move {
-                let client_stream = TcpStream::connect(addr).await.unwrap();
+                match TcpStream::connect(addr).await {
+                    Ok(client_stream) => {
+                        let (mut sender, conn) =
+                            hyper::client::conn::handshake(client_stream).await?;
+                        tokio::task::spawn(async move {
+                            if let Err(err) = conn.await {
+                                println!("Connection failed: {:?}", err);
+                            }
+                        });
 
-                let (mut sender, conn) = hyper::client::conn::handshake(client_stream).await?;
-                tokio::task::spawn(async move {
-                    if let Err(err) = conn.await {
-                        println!("Connection failed: {:?}", err);
+                        sender.send_request(req).await
                     }
-                });
-
-                sender.send_request(req).await
+                    Err(e) => Ok(Response::builder()
+                        .status(502)
+                        .body(Body::from(format!(
+                            "failed to connect to upstream server\n{}\n\n{VERSION_STRING}",
+                            e
+                        )))
+                        .unwrap()),
+                }
             }
         });
 
         tokio::task::spawn(async move {
             if let Err(err) = hyper::server::conn::Http::new()
+                .http1_preserve_header_case(true)
+                .http1_title_case_headers(true)
                 .serve_connection(stream, service)
                 .await
             {
-                println!("Failed to serve the connection: {:?}", err);
+                error!("failed to serve the connection: {:?}", err);
             }
         });
     }
