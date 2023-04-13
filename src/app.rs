a15c1b2839fbca43073148d643591eefe49a0e1c
Lennart Kloock <lennart.kloock@protonmail.com>
2023-04-13 22:05:23 +0200

    Add configurable css custom properties

--- src/app.rs
+++ src/app.rs
@@ -1,17 +1,19 @@
 use axum::{Router, routing::get};
+use axum::extract::State;
+use crate::config::CustomCss;
 
 mod templates;
 
-pub fn router() -> Router {
+pub fn router() -> Router<Option<CustomCss>> {
     Router::new()
         .route("/login", get(login))
         .route("/register", get(register))
 }
 
-async fn login() -> templates::Login {
-    templates::Login {}
+async fn login(State(custom_css): State<Option<CustomCss>>) -> templates::Login {
+    templates::Login { custom_css }
 }
 
-async fn register() -> templates::Register {
-    templates::Register {}
+async fn register(State(custom_css): State<Option<CustomCss>>) -> templates::Register {
+    templates::Register { custom_css }
 }
--- src/app/templates.rs
+++ src/app/templates.rs
@@ -1,7 +1,13 @@
+use crate::config::CustomCss;
+
 #[derive(askama::Template)]
 #[template(path = "login.html")]
-pub struct Login {}
+pub struct Login {
+    pub custom_css: Option<CustomCss>,
+}
 
 #[derive(askama::Template)]
 #[template(path = "register.html")]
-pub struct Register {}
+pub struct Register {
+    pub custom_css: Option<CustomCss>,
+}
